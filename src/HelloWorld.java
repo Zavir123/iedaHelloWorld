b24292c749c94c568032b5d9c94ef5b96815b754
zavir <1134938968@qq.com>
2020-07-20 09:30:31 +0800

    第一次提交

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * @author Zavir
+ * @create 2020-07-20 上午 9:26
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
